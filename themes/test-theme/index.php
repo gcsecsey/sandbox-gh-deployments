da4b5f561c5ccf72829c40186eaaa92473389d3b
Gergely Csécsey <gcsecsey@gmail.com>
2026-01-16 11:52:39 +0000

    Revert "remove directories"
    This reverts commit 1df33b8387cf736ae77b3ea629ddde38d997c71f.

---
+++ themes/test-theme/index.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Template for displaying the test theme
+ * 
+ * This theme is used to test protected paths during GitHub deployments
+ */
+
+get_header(); ?>
+
+<main>
+    <h1>Test Theme - Protected Path Test</h1>
+    <p>This theme was deployed from the sandbox-gh-deployments repository.</p>
+</main>
+
+<?php get_footer(); ?>
